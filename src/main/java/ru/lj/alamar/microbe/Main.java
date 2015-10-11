21f286b759271e72709b3fc2584f5e6a9f5bc459
Ilya Kasnacheev <ilya.kasnacheev@gmail.com>
2015-10-11 17:13:52 +0700

    Some optimizations, polyploidy.

--- src/main/java/ru/lj/alamar/microbe/Main.java
+++ src/main/java/ru/lj/alamar/microbe/Main.java
@@ -11,6 +11,7 @@ import ru.yandex.bolts.collection.Tuple2List;
  */
 public class Main {
     public static int MICROBES = 100000;
+    public static int CHROMOSOMES = 1;
     public static int GENES = 100;
     public static float GENE_MUTATION_CHANCE = 0.00864f;
     public static float MUTATION_POSITIVE_CHANCE = 0.1f;
@@ -19,12 +20,12 @@ public class Main {
     public static float LUCK_RATIO = 0.3f;
 
     public static void main(String[] args) {
-        Random r = new Random();
+        Random r = new Random(1444556354);
         ListF<Microbe> microbes = Cf.arrayList();
         for (int i = 0; i < MICROBES; i++) {
-            microbes.add(new Microbe(GENES));
+            microbes.add(new Microbe(CHROMOSOMES, GENES));
         }
-        for (int s = 0; s < 500; s++) {
+        for (int s = 0; s < 1000; s++) {
             float totalFitness = 0f;
             for (Microbe microbe : microbes) {
                 microbe.mutate(r, GENE_MUTATION_CHANCE, NEGATIVE_EFFECT, MUTATION_POSITIVE_CHANCE, POSITIVE_EFFECT);
@@ -32,7 +33,7 @@ public class Main {
             }
             float avgFitness = totalFitness / (float) microbes.size();
             microbes = selectOffspring(r, microbes);
-            System.out.println(microbes.size() + "\t" + avgFitness);
+            System.out.println(s + "\t" + microbes.size() + "\t" + avgFitness);
         }
     }
 
@@ -40,8 +41,9 @@ public class Main {
         Tuple2List<Float, Microbe> withFitnessAndLuck = Tuple2List.arrayList();
         for (Microbe microbe : population) {
             if (microbe.isDead()) continue;
-            withFitnessAndLuck.add(microbe.fitness() * (1f - LUCK_RATIO) + r.nextFloat() * LUCK_RATIO, microbe);
-            withFitnessAndLuck.add(microbe.fitness() * (1f - LUCK_RATIO) + r.nextFloat() * LUCK_RATIO, microbe.replicate());
+            float fitness = microbe.fitness();
+            withFitnessAndLuck.add(fitness * (1f - LUCK_RATIO) + r.nextFloat() * LUCK_RATIO, microbe);
+            withFitnessAndLuck.add(fitness * (1f - LUCK_RATIO) + r.nextFloat() * LUCK_RATIO, microbe.replicate(r));
         }
         return withFitnessAndLuck.sortBy1().reverse().get2().take(population.size());
     }
--- src/main/java/ru/lj/alamar/microbe/Microbe.java
+++ src/main/java/ru/lj/alamar/microbe/Microbe.java
@@ -1,5 +1,6 @@
 package ru.lj.alamar.microbe;
 
+import java.util.Collections;
 import java.util.Random;
 
 import ru.yandex.bolts.collection.Cf;
@@ -12,24 +13,36 @@ public class Microbe {
     // fitness: 0.0 to 1.0
     public static final float NORMAL_FITNESS = 0.9f;
     public static final float ALIVE_FITNESS = 0.5f;
-    private float[] genes;
+    private float[][] chromosomes;
+    private float fitness = -1f;
 
-    protected Microbe(float[] parentGenesCopy) {
-        genes = parentGenesCopy;
+    protected Microbe(float[][] inheritedChromosomes) {
+        chromosomes = inheritedChromosomes;
     }
 
-    public Microbe(int numGenes) {
-        genes = new float[numGenes];
-        for (int i = 0; i < numGenes; i++) {
-            genes[i] = NORMAL_FITNESS;
+    public Microbe(int ploidy, int numGenes) {
+        chromosomes = new float[ploidy][numGenes];
+        for (float[] chromosome : chromosomes) {
+           for (int g = 0; g < numGenes; g++) {
+               chromosome[g] = NORMAL_FITNESS;
+           }
         }
     }
 
     public float fitness() {
+        if (fitness >= 0) return fitness;
+
         float result = 1.0f;
-        for (int i = 0; i < genes.length; i++) {
-            result *= (genes[i] / NORMAL_FITNESS);
+        for (int g = 0; g < chromosomes[0].length; g++) {
+            float geneFitness = chromosomes[0][g];
+            for (int p = 1; p < chromosomes.length; p++) {
+                if (chromosomes[p][g] > geneFitness) {
+                    geneFitness = chromosomes[p][g];
+                }
+            }
+            result *= (geneFitness / NORMAL_FITNESS);
         }
+        fitness = result;
         return result;
     }
 
@@ -38,17 +51,30 @@ public class Microbe {
     }
 
     public void mutate(Random r, float geneMutationChance, float negativeModifier, float mutationPositiveChance, float positiveModifier) {
-        for (int i = 0; i < genes.length; i++) {
-            if (r.nextFloat() > geneMutationChance) continue;
-            if (r.nextFloat() < mutationPositiveChance) {
-                genes[i] = Math.min(1f, genes[i] + positiveModifier);
-            } else {
-                genes[i] = Math.max(0f, genes[i] - negativeModifier);
+        for (float[] chromosome : chromosomes) {
+            for (int g = 0; g < chromosome.length; g++) {
+                if (r.nextFloat() > geneMutationChance) continue;
+                if (r.nextFloat() < mutationPositiveChance) {
+                    chromosome[g] = Math.min(1f, chromosome[g] + positiveModifier);
+                } else {
+                    chromosome[g] = Math.max(0f, chromosome[g] - negativeModifier);
+                }
             }
         }
     }
 
-    public Microbe replicate() {
-        return new Microbe(genes.clone());
+    private static float[][] OF_CHROMOSOMES = new float[0][0];
+    // XXX Mutates (not in biological sense :)
+    public Microbe replicate(Random r) {
+        ListF<float[]> doubled = Cf.arrayList();
+        for (float[] chromosome : chromosomes) {
+            doubled.add(chromosome.clone());
+            doubled.add(chromosome);
+        }
+        // XXX Require Random r
+        Collections.shuffle(doubled, r);
+        chromosomes = doubled.take(chromosomes.length).toArray(OF_CHROMOSOMES);
+        fitness = -1f;
+        return new Microbe(doubled.drop(chromosomes.length).toArray(OF_CHROMOSOMES));
     }
 }
