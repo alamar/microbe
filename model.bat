15bdc31811274768359fe08b355323dd8d01265e
Ilya Kasnacheev <ilya.kasnacheev@gmail.com>
2015-12-09 16:53:45 +0300

    Add -Xmx

--- model.bat
+++ model.bat
@@ -1,1 +1,1 @@
-java -classpath target/microbe-1.0-SNAPSHOT.jar;lib/ru.yandex-bolts-jar-20121016122946.jar;target/dependency/* ru.lj.alamar.microbe.Model %*
+java -Xmx1792M -classpath target/microbe-1.0-SNAPSHOT.jar;lib/ru.yandex-bolts-jar-20121016122946.jar;target/dependency/* ru.lj.alamar.microbe.Model %*
