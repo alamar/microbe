45a27f64b60a930c8de67bf8f98f5ae8879f1d57
Ilya Kasnacheev <ilya.kasnacheev@gmail.com>
2015-12-15 14:37:30 +0300

    Ask for model if there isn't one.

--- model.bat
+++ model.bat
@@ -1,1 +1,5 @@
-java -Xmx1792M -classpath target/microbe-1.0-SNAPSHOT.jar;lib/ru.yandex-bolts-jar-20121016122946.jar;target/dependency/* ru.lj.alamar.microbe.Model %*
+if [%1] == [] (
+set /p model= "Enter model:"
+)
+
+java -Xmx1024M -classpath target/microbe-1.0-SNAPSHOT.jar;lib/ru.yandex-bolts-jar-20121016122946.jar;target/dependency/* ru.lj.alamar.microbe.Model %model% %*
